bda94581b185fecfc37925fb096ca1d3e32e33b1
KJS-alt <sh170kw@naver.com>
2025-10-17 17:08:58 +0900

    1017

---
+++ src/main/java/com/example/demo/exthym/ExThymApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo.exthym;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExThymApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExThymApplication.class, args);
+    }
+
+}
